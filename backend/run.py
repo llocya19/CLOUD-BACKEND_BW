b99c1e3c054b8dfca7aa3eeb7e6b7be57ba34f3c
Llocya Saravia <llocyasaraviajuan@gmail.com>
2025-06-30 10:17:55 -0500

    Ajusre de run.py para puerto en Fly.io

--- backend/run.py
+++ backend/run.py
@@ -9,4 +9,6 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Configura para Fly: Puerto 8080 y debug desactivado
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
